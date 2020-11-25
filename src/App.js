b872de365eb16bcfca52da97b14341fe0e956013
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-11-25 14:00:26 -0500

    working on portfolio section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
-import profile from './images/profilepic.jpeg'
+import profile from './images/profilepic.jpeg';
+import newsapp from './images/portfolio-newsapp.png';
+import picture from './images/portfolio-pictureperfect.png';
+
 
 function App() {
   return (
@@ -26,6 +29,32 @@ function App() {
         <div style={{height: '500px'}}className="col-6 bio-right">
           <p>So a little about me, I was born in Mexico but I was raised in Greenville, SC. I love anime, music, playing video games, finding new restaurants, but most importantly, I LOVE TO CODE. Under the guidance of seasoned instructors and staff at the Carolina Code School, I was given not only the skills necessary to be a successful Full Stack Developer, but also the tools to learn. Armed with these new skills, I have immersed myself fully into the software development world and have continued to learn and improve myself not only as a programmer, but also as a student in the never-ending journey of software development.</p>
         </div>
+      </div>
+    </section>
+    <section id="portfolio" className="portfolio">
+      <div className="newsapp">
+        <div className="display">
+          <a target="_blank" rel="noreferrer" href="https://django-fullstack-newsapp.herokuapp.com/">
+            <img src={newsapp} alt="newsapp"/>
+          </a>
+        </div>
+      </div>
+      <div className="description">
+        <h2>News app</h2>
+        <p>A full stack app for uploading news articles.</p>
+        <h4 style={{marginBottom: '20px'}}><a target="_blank" rel="noreferrer" href="https://github.com/BrianCambron/Django_Fullstack_Newsapp">View Code</a></h4>
+      </div>
+      <div className="picture-perfect">
+        <div className='display'>
+          <a target="_blank" rel="noreferrer" href="https://briancambron.github.io/pixel-perfect/">
+            <img src={picture} alt="brian's pixel perfect"/>
+          </a>
+        </div>
+      </div>
+      <div className='description'>
+        <h2>Pixel Perfect</h2>
+        <p>A mockup recreation using HTML and CSS.</p>
+        <h4 style={{marginBottom: '20px'}}><a target="_blank" rel="noreferrer" href="https://github.com/BrianCambron/pixel-perfect">View Code</a></h4>
       </div>
     </section>
     <section id='contact'>
